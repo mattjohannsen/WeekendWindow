d0fc25ab6605e4da3df5d2d958b08f1d3637fb73
mattjohannsen <majohann@hotmail.com>
2020-04-03 17:06:19 -0500

    ASP NET MVC original

---
+++ WeekendWindow/Controllers/WarriorsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WeekendWindow.Data;
+using WeekendWindow.Models;
+
+namespace WeekendWindow.Controllers
+{
+    public class WarriorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WarriorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Warriors
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var applicationDbContext = _context.Warriors.Include(w => w.IdentityUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Warriors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warrior = await _context.Warriors
+                .Include(w => w.IdentityUser)
+                .FirstOrDefaultAsync(m => m.WarriorId == id);
+            if (warrior == null)
+            {
+                return NotFound();
+            }
+
+            return View(warrior);
+        }
+
+        // GET: Warriors/Create
+        public IActionResult Create()
+        {
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Warriors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WarriorId,IdentityUserId,FirstName,LastName,Address,City,State,Zipcode")] Warrior warrior)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(warrior);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", warrior.IdentityUserId);
+            return View(warrior);
+        }
+
+        // GET: Warriors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warrior = await _context.Warriors.FindAsync(id);
+            if (warrior == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", warrior.IdentityUserId);
+            return View(warrior);
+        }
+
+        // POST: Warriors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WarriorId,IdentityUserId,FirstName,LastName,Address,City,State,Zipcode")] Warrior warrior)
+        {
+            if (id != warrior.WarriorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(warrior);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WarriorExists(warrior.WarriorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", warrior.IdentityUserId);
+            return View(warrior);
+        }
+
+        // GET: Warriors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warrior = await _context.Warriors
+                .Include(w => w.IdentityUser)
+                .FirstOrDefaultAsync(m => m.WarriorId == id);
+            if (warrior == null)
+            {
+                return NotFound();
+            }
+
+            return View(warrior);
+        }
+
+        // POST: Warriors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var warrior = await _context.Warriors.FindAsync(id);
+            _context.Warriors.Remove(warrior);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WarriorExists(int id)
+        {
+            return _context.Warriors.Any(e => e.WarriorId == id);
+        }
+    }
+}
---
+++ WeekendWindow/Data/Migrations/20200403194647_initialweekendwindow.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeekendWindow.Data.Migrations
+{
+    public partial class initialweekendwindow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "2c40beea-a3e6-42e3-9e90-1574d555fabb", "b4a6e2de-a0f4-4cde-8c75-c62eba351211", "Warrior", "WARRIOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c40beea-a3e6-42e3-9e90-1574d555fabb");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
